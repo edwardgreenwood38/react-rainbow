03edeee466cce9467b3ff04b59e3566f71f0c36d
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-02-19 13:46:16 -0500

    step 3 completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  let colors = ['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red']
   return (
     <div className="App">
       
