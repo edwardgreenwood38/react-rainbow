e10a31dbeaa41fb30121b21b6ecf9f56871e89a5
Edward E Greenwood <edwardgreenwood38@gmail.com>
2024-02-19 14:11:59 -0500

    step 4 - ColorBlock completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 
 import './App.css';
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import ColorBlock from './ColorBlock';
+
 
 function App() {
-  let colors = ['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red']
+  let colors = ['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red'];
+  
+  // let colorMap = colors.map((color, i) => {
+  //   return (
+  //     <ColorBlock color={color} />
+  //   )
+  // });
+  
   return (
     <div className="App">
-      
+      {colors.map((color, i) => (
+        <ColorBlock key={i} color={color} />
+      ))}
     </div>
   );
 }
